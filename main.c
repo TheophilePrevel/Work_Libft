d8dad61863d470c04630e4933be3a66ba31546de
tprevel <TheophilePrevel>
2019-11-06 18:05:45 +0100

    main pour travail sur le ft_split

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include "libft.h"
 
-int		main(void)
+int		main(int ac, char **av)
 {
-	printf("%s\n",ft_itoa(-2147483648));
+	int i;
+	char **test;
+
+	i = 0;
+	if (ac == 3)
+	{
+		test = ft_split(av[1], av[2][0]);
+		while (test[i])
+			printf("%s\n",test[i++]);
+	}
 	return (0);
 }
